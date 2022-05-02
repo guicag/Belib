97d5666c584e1a26d7e6545f4f02e86b190b73e4
clementjrt <clementjouret@hotmail.fr>
2022-05-02 22:00:23 +0200

    Start of details view

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -8,10 +8,8 @@ import '../providers/belib_api_provider.dart';
 import '../providers/location_provider.dart';
 import '../models/belib_model.dart';
 import 'infos_markers.dart';
-import 'package:belib/database/commentBox.dart';
 import 'details_markers.dart';
 import 'package:belib/database/commentBox.dart';
-import 'add_comments.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -256,19 +254,26 @@ class _MyAppState extends State<MyApp> {
                                                     icon: Icon(Icons.feed_outlined),
                                                     label: Text("View details"),
                                                     onPressed: () => {
+                                                      // TODO
                                                       Navigator.push(
                                                         context,
                                                         MaterialPageRoute(builder: (context) => DetailsMarker(snapshot.data!.records![i].fields!)),
                                                       )
+                                                      /*Fluttertoast.showToast(
+                                                        msg: "View details",
+                                                        toastLength: Toast.LENGTH_SHORT,
+                                                        gravity: ToastGravity.BOTTOM,
+                                                      )*/
                                                     }
                                                 ),
                                                 TextButton.icon(
                                                     icon: Icon(Icons.add),
                                                     label: Text("Add comment"),
                                                     onPressed: () => {
-                                                      Navigator.push(
-                                                        context,
-                                                        MaterialPageRoute(builder: (context) => AddComment(snapshot.data!.records![i].recordid!)),
+                                                      Fluttertoast.showToast(
+                                                        msg: "Add details",
+                                                        toastLength: Toast.LENGTH_SHORT,
+                                                        gravity: ToastGravity.BOTTOM,
                                                       )
                                                     }
                                                 ),
