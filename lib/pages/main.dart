e1f2154c0d2899d7ed55a76821bb2183f6b975a5
Guillaume Cagniard <guillaume.cagniard@renault.com>
2022-05-11 11:23:18 +0200

    feat: add mark + change the markers colors

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -88,6 +88,52 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  Color calculateRateMarker(Fields charger) {
+    double mark;
+    int nbrPoints = 0;
+    if (charger.statutPdc == "En service") {
+      nbrPoints++;
+    }
+    if (charger.paiementCb == "true") {
+      nbrPoints++;
+    }
+    if (charger.priseTypeEf == "True") {
+      nbrPoints++;
+    }
+    if (charger.priseType2 == "True") {
+      nbrPoints++;
+    }
+    if (charger.priseType3 == "True") {
+      nbrPoints++;
+    }
+    if (charger.priseTypeComboCcs == "True") {
+      nbrPoints++;
+    }
+    if (charger.priseTypeChademo == "True") {
+      nbrPoints++;
+    }
+    if (charger.reservation == "true") {
+      nbrPoints++;
+    }
+    if (charger.priseTypeAutre == "True") {
+      nbrPoints++;
+    }
+    if (charger.paiementActe == "true") {
+      nbrPoints++;
+    }
+    if (charger.stationDeuxRoues == "true") {
+      nbrPoints++;
+    }
+    mark = (nbrPoints / 11) * 10;
+    if (mark <= 3.0) {
+      return Colors.red;
+    } else if (mark <= 5.0) {
+      return Colors.orange;
+    } else {
+      return Colors.green;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
