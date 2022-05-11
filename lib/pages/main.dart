932c4f18bd9902349a006ebfc1336168ff126298
Guillaume Cagniard <guillaume.cagniard@renault.com>
2022-05-11 14:27:29 +0200

    feat: add noSQL database

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -3,14 +3,18 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
+import '../models/comment_model.dart';
 import '../providers/belib_api_provider.dart';
 import '../providers/location_provider.dart';
 import '../models/belib_model.dart';
 import 'infos_markers.dart';
+import 'package:belib/database/commentBox.dart';
 import 'details_markers.dart';
 import 'add_comments.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await CommentBox.init();
   runApp(MaterialApp(
       home: MyApp()
   ));
@@ -34,6 +38,8 @@ class _MyAppState extends State<MyApp> {
     super.initState();
     futureBelib = fetchBelib();
     futureLocation = determinePosition();
+    //CommentBox.box.add(Comments(id:"test", title: "titre", message: "message"));
+    //print(CommentBox.box.getAt(0).id);
   }
 
   @override
