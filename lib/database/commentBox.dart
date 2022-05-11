c3ceae85fc55bf57b1d45e19fa6120d0fe55f2f1
Guillaume Cagniard <guillaume.cagniard@renault.com>
2022-05-11 16:54:50 +0200

    add comment

--- lib/database/commentBox.dart
+++ lib/database/commentBox.dart
@@ -12,26 +12,5 @@ class CommentBox {
     Hive.init(dir.path);
     Hive.registerAdapter(CommentsAdapter());
     box = await Hive.openBox("commentBox");
-
-    /*var values = box.values;
-    if (values.isEmpty) {
-      box.putAll(Map.fromIterable(commentsList, key: (e) => e.key(), value: (e) => e));
-    }*/
   }
-
-  static void addComment(String id, String title, String message) {
-    box.add(Comments(id: id, title: title, message: message));
-  }
-
-  /*static List<Comments> getComments(String id) {
-    List<Comments> res = [];
-    for (int i = 0; i < commentsList.length; i++) {
-      if (commentsList[i].id == id) {
-        res.add(commentsList[i]);
-      }
-    }
-    return res;
-  }*/
-
-  static final List<Comments> commentsList = [];
 }
